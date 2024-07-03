7a7db633478a6db774d1562935409ab72242af07
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2024-07-02 21:21:50 -0300

    Update util_config.sh

--- bin/util_config.sh
+++ bin/util_config.sh
@@ -12,16 +12,16 @@ check_root "$1"
 hosts_conf()
 {
 	
-	echo "127.0.0.1				localhost" 							> /etc/hosts
-	echo "192.168.0.1			router.lls.net.br		router"		>> /etc/hosts
-	echo "192.168.0.100		    dell.lls.net.br			dell" 		>> /etc/hosts
-	echo "192.168.0.110		    asus.lls.net.br			asus" 		>> /etc/hosts
- 	echo "192.168.0.7		    leandro.lls.net.br		leandro"	>> /etc/hosts
- 	echo "192.168.0.4		    wanda.lls.net.br		wanda"		>> /etc/hosts
- 	echo "192.168.0.3		    tv.lls.net.br			tv"			>> /etc/hosts
- 	echo "192.168.0.2		    camera.lls.net.br		camera"		>> /etc/hosts
-	echo "18.228.3.148			funchal.lls.net.br		funchal" 	>> /etc/hosts
-	echo "18.228.3.148			repository.lls.net.br	repository"	>> /etc/hosts
+	echo "127.0.0.1			localhost" 							> /etc/hosts
+	echo "192.168.0.1		router.lls.net.br		router"		>> /etc/hosts
+	echo "192.168.0.100		dell.lls.net.br			dell" 		>> /etc/hosts
+	echo "192.168.0.200		3green.lls.net.br		3green" 	>> /etc/hosts
+ 	echo "192.168.0.7		leandro.lls.net.br		leandro"	>> /etc/hosts
+ 	echo "192.168.0.4		wanda.lls.net.br		wanda"		>> /etc/hosts
+ 	echo "192.168.0.3		tv.lls.net.br			tv"		>> /etc/hosts
+ 	echo "192.168.0.2		camera.lls.net.br		camera"		>> /etc/hosts
+	echo "18.228.3.148		funchal.lls.net.br		funchal" 	>> /etc/hosts
+	echo "18.228.3.148		repository.lls.net.br		repository"	>> /etc/hosts
 	echo "208.67.222.222		opendns.lls.net.br		opendns" 	>> /etc/hosts
 	echo "208.67.220.220		opendns2.lls.net.br		opendns2" 	>> /etc/hosts
 	
@@ -59,76 +59,12 @@ scripts_conf()
 	DIR_BIN="/usr/bin"
 	
 	FILES_SET=(
-		"change_brightness.sh"
-		"stream_record.sh"
 		"cloud_connect.sh"
-		"print_screen.sh"
-		"video_copy.sh"
 		"jquery-lls.sh"
 		"terminal.sh"
-		"youtube.sh"
-		"crack.sh"
 	)
 	
 	update_files "Configure" "${DIR_BIN}" "scripts"
-	
-}
-
-pixmaps_conf()
-{
-	
-	APP_NAME="pixmaps"
-	
-	FILES_SET=(
-		"vlc.xpm"
-		"kodi.xpm"
-		"tools.xpm"
-		"thunar.xpm"
-		"reboot.xpm"
-		"office.xpm"
-		"network.xpm"
-		"firefox.xpm"
-		"mousepad.xpm"
-		"chromium.xpm"
-		"shutdown.xpm"
-		"mate-calc.xpm"
-		"developer.xpm"
-		"ristretto.xpm"
-		"anonymous.xpm"
-		"teamviewer.xpm"
-		"homeoffice.xpm"
-		"multimedia.xpm"
-		"torbrowser.xpm"
-		"print_screen.xpm"
-	)
-	
-	update_files "Configure" "/usr/share/${APP_NAME}" "images/${APP_NAME}"
-	
-	update_file "wallpaper.png" "/usr/share/backgrounds" "images"
-	
-	cp -fv images/wallpaper.png /usr/share/images/fluxbox/debian-dark.png
-	cp -fv images/wallpaper.png /usr/share/images/fluxbox/ubuntu-dark.png
-	cp -fv images/wallpaper.png /usr/share/images/fluxbox/ubuntu-light.png
-	cp -fv images/wallpaper.png /usr/share/images/fluxbox/fluxbox.png
-	
-}
-
-grub_conf()
-{
-	
-	sudo apt-get -y install grub-customizer
-	
-	sudo cp -fv images/wallpaper.png /usr/share/grub/themes/kali/grub-4x3.png
-	sudo cp -fv images/wallpaper.png /usr/share/grub/themes/kali/grub-16x9.png
-	
-	sudo grub-customizer
-	
-}
-
-lightdm_conf()
-{
-	
-	sudo lightdm-gtk-greeter-settings
 	
 }
 
@@ -142,23 +78,13 @@ case "$1" in
 	scripts)
 		scripts_conf
 		;;
-	pixmaps)
-		pixmaps_conf
-		;;
-	grub)
-		grub_conf
-		;;
-	lightdm)
-		lightdm_conf
-		;;
 	all)
 		sudo_conf
 		hosts_conf
 		scripts_conf
-		pixmaps_conf
 		;;
 	*)
-		echo "Use: $0 {all|sudo|grub|hosts|scripts|pixmaps|lightdm}"
+		echo "Use: $0 {all|sudo|hosts|scripts}"
 		exit 1
 		;;
 esac
