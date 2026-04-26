a6da0c906972d44646e681e82ffa20f9883a9b1c
lls <lls.homeoffice@gmail.com>
2026-04-26 20:08:20 -0300

    Add files remotely

--- util/install/docker.sh
+++ util/install/docker.sh
@@ -19,33 +19,13 @@ docker_edit()
 docker_install()
 {
 	
-	# Remova versões antigas
-	sudo apt-get -y remove docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc
-	
-	# Configurar o repositório oficial
-	sudo apt-get update
-	sudo apt-get -y install ca-certificates curl
-	sudo install -m 0755 -d /etc/apt/keyrings
-
-	# Adicione a chave GPG oficial da Docker
-	sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-	sudo chmod a+r /etc/apt/keyrings/docker.asc
-	
-	# Adicione o repositório às fontes do apt
-	echo \
-	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-	  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	
-	# Instalar o Docker Engine
-	sudo apt update
-	sudo apt-get -y install docker.io docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+	sudo apt install docker.io docker-compose -y
 	
 	# Verificar a instalação
 	sudo docker run hello-world
 	
 	# Executar sem sudo
-	sudo usermod -aG docker $USER
+	sudo usermod -aG docker ${USER}
 	
 	docker_version
 	
