a1bfed3aae8e3b2ba2a8b8a55c19664046e2987c
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2026-07-25 18:48:07 -0300

    Update docker.sh

--- util/install/docker.sh
+++ util/install/docker.sh
@@ -23,10 +23,15 @@ docker_install()
 	sudo apt-get install -y ca-certificates curl gnupg
 
 	sudo install -m 0755 -d /etc/apt/keyrings
-
+	
+	sudo rm -fv /etc/apt/keyrings/docker.gpg
+	
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	
 	sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
+	sudo rm -fv /etc/apt/sources.list.d/docker.list
+	
 	echo \
 	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
 	  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
