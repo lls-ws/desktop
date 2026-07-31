bd719ad079a80c50aabc47014802f08bd91a979f
Leandro Luiz <69089565+lls-ws@users.noreply.github.com>
2026-07-31 12:53:02 -0300

    Update server_config.sh

--- bin/server_config.sh
+++ bin/server_config.sh
@@ -40,9 +40,6 @@ net_conf()
 	
 	echo "Retorna 1 para conectado:"
 	cat /sys/class/net/${INTERFACE}/carrier
-
-	echo "Desligando a interface:"
-	sudo ip link set ${INTERFACE} down
 	
 	echo "Subir a interface:"
 	sudo ip link set ${INTERFACE} up
