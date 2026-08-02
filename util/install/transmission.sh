f7fb331bf9e3a8df39440f307287d101a9149805
lls <lls.homeoffice@gmail.com>
2026-08-02 00:04:08 -0300

    Add files remotely

--- util/install/transmission.sh
+++ util/install/transmission.sh
@@ -143,7 +143,7 @@ transmission_log()
 apparmor_conf()
 {
 	
-	transmission_copy "transmission" "etc/apparmor.d/local"
+	#transmission_copy "transmission" "etc/apparmor.d/local"
 	
 	FILE_APPARMOR="usr.bin.transmission-daemon"
 	DIR_APPARMOR="etc/apparmor.d"
