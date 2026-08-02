431b750e53f40f7c1082e7ba9ceb958dde8638d4
lls <lls.homeoffice@gmail.com>
2026-08-02 16:16:21 -0300

    Add files remotely

--- util/install/apparmor.sh
+++ util/install/apparmor.sh
@@ -11,6 +11,13 @@ clear
 
 check_root "$1"
 
+apparmor_install()
+{
+	
+	apt -y install ${NAME_APP} apparmor-utils
+	
+}
+
 apparmor_conf()
 {
 	
@@ -33,32 +40,38 @@ apparmor_remove()
 {
 	
 	echo "Desative e pare o serviço do AppArmor"
-	sudo systemctl stop apparmor
-	sudo systemctl disable apparmor
+	sudo systemctl stop ${NAME_APP}
+	sudo systemctl disable ${NAME_APP}
 	
 	echo "Remova os pacotes do AppArmor do sistema"
-	sudo apt purge -y apparmor apparmor-utils
+	sudo apt purge -y ${NAME_APP} apparmor-utils
 	
 	echo "Limpe as pastas de configurações e perfis residuais"
-	sudo rm -rf /etc/apparmor.d/ /var/lib/apparmor/
+	sudo rm -rf /etc/apparmor.d/ /var/lib/${NAME_APP}/
 	
 	echo "Reinicie o servidor para descarregar o módulo do kernel"
 	sudo reboot
 	
 }
 
+NAME_APP="apparmor"
+
 case "$1" in
-	remove)
-		apparmor_remove
+	install)
+		apparmor_install
 		;;
 	conf)
 		apparmor_conf
 		;;
+	remove)
+		apparmor_remove
+		;;
   	all)
+  		apparmor_install
   		apparmor_conf
   		;;
 	*)
-		echo "Use: $0 {all|remove|conf}"
+		echo "Use: $0 {all|install|conf|remove}"
 		exit 1
 		;;
 esac
